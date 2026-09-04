20e0fe38e4cce78896495e9698faf768a8fad6ad
varshithapusalapati <varshithapusalapati@gmail.com>
2026-09-04 14:22:01 +0530

    membership operator

---
+++ membership.py
@@ -0,0 +1,8 @@
+#membership operator
+a=[1,2,3]
+result=2 in a #in membership operator
+print("result of 2 in a is",result)
+
+a=[1,2,3]
+result=5 in a #in membership operator
+print("result of 5 in a is",result)

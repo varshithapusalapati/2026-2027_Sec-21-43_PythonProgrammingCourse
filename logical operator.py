f5bfb1ecf54ecac46353cfd66301c2d2e2a7f62d
varshithapusalapati <varshithapusalapati@gmail.com>
2026-09-04 14:21:41 +0530

    identity operator

---
+++ logical operator.py
@@ -0,0 +1,22 @@
+#python Logical Operators Exercise
+a = 20
+result = a > 2 and a < 10 #logical and operator
+print("result of",a,"> 2 and", a ,"< 10:",result)
+
+#python Logical Operators Exercise
+a = 5
+result = a > 2 and a < 10 #logical and operator
+print("result of",a,"> 2 and", a ,"< 10:",result)
+
+
+#python Logical Operators Exercise
+"""a = 15
+result = a > 2 or a < 10 #logical and operator
+print("result of",a,"> 2 or", a ,"< 10:",result)"""
+
+#python Logical Operators Exercise
+a = 5
+result = a > 2 not 5< 10 #logical and operator
+print("result of",a,"> 2 not", a ,"< 10:",result)
+
+

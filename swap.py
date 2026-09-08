4085148d769b6d9bbfec6fddf2a71c6be641e806
varshithapusalapati <varshithapusalapati@gmail.com>
2026-09-08 13:25:44 +0530

    swapping two numbers

---
+++ swap.py
@@ -0,0 +1,10 @@
+#swap two numbers
+a = 5
+b = 10
+print("Before swapping: a =", a, "b =", b)
+# Swapping using a temporary variable
+temp = a
+a = b
+b = temp
+print("After swapping: a =", a, "b =", b)
+

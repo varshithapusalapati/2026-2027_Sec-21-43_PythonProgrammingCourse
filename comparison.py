dd5229b5802f7aa51b0bf18f7a35c629060fa0c8
varshithapusalapati <varshithapusalapati@gmail.com>
2026-09-08 13:20:17 +0530

    comparison operator

---
+++ comparison.py
@@ -0,0 +1,23 @@
+#Python comparision operators excercise
+a=10
+b=5
+result=a>b #greater than comparision operator
+print("result of",a,">",b,"is:",result)
+
+result=a<b #less than comparision operator
+print("result of",a,"<",b,"is:",result)
+
+"""a=15
+b=15
+result=a==b #equal for comparision operator 
+print("result of",a,"==",b,"is :",result)"""
+
+"""a=10
+b=15
+result=a!=b #not equal for comparision operator
+print("result of",a,"!=",b,"is :",result)"""
+
+a=15
+b=10
+result=a>=b #greater than equal to comparision operator
+print("result of",a,">=",b,"is :",result) 

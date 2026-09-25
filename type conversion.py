6f5436e665c49415a2d84ace80e6236c0ba22daa
varshithapusalapati <varshithapusalapati@gmail.com>
2026-09-25 16:02:39 +0530

    type conversion

---
+++ type conversion.py
@@ -0,0 +1,26 @@
+# Demonstrating Type Conversion
+
+# Integer to Float
+a = 10
+b = float(a)
+print("Integer to Float:", b)
+
+# Float to Integer
+c = 10.5
+d = int(c)
+print("Float to Integer:", d)
+
+# Integer to String
+e = 25
+f = str(e)
+print("Integer to String:", f)
+
+# String to Integer
+g = "50"
+h = int(g)
+print("String to Integer:", h)
+
+# String to Float
+i = "12.5"
+j = float(i)
+print("String to Float:", j)

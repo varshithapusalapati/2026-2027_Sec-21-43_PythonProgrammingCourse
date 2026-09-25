55900c1fd9e352b1040516de06ebef2f39d71345
varshithapusalapati <varshithapusalapati@gmail.com>
2026-09-25 15:59:36 +0530

    operator precedence

---
+++ operator.py
@@ -0,0 +1,16 @@
+# Demonstrating Operator Precedence and Associativity
+
+# Operator Precedence
+result1 = 10 + 5 * 2
+print("10 + 5 * 2 =", result1)
+
+# Using brackets to change precedence
+result2 = (10 + 5) * 2
+print("(10 + 5) * 2 =", result2)
+
+# Operator Associativity
+result3 = 20 - 5 - 3
+print("20 - 5 - 3 =", result3)
+
+result4 = 2 ** 3 ** 2
+print("2 ** 3 ** 2 =", result4)
